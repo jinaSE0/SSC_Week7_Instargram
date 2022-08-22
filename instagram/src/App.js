bab1f348ad159d8d04402a2e6a72ff8c3e027dd5
jinaSE0 <109025674+jinaSE0@users.noreply.github.com>
2022-08-22 15:06:17 +0900

    Merge branch 'main' into jh

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,8 +1,25 @@
+// import Router from "./shared/Router";
+// import { useForm } from 'react-hook-form' 
+
+
+// function App() {
+// const { register, handleSubmit, watch, formState: { errors } } = useForm();
+// const onSubmit = data => console.log(data);
+   
+
+//     return (
+//         <Router />
+//     );
+// }
+
+// export default App;
+
 
 import Router from "./shared/Router";
 
-
 function App() {
+   
+
     return (
         <Router />
     );
